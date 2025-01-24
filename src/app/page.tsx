ff1589a285f45721a1ac3b6a61e399cc8bfb2231
ralzmr <143031384+ralzmr@users.noreply.github.com>
2025-01-24 14:49:06 -0500

    shadcn dependency added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -22,6 +23,10 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
+        <Button>
+          Hi
+        </Button>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
